5cf9a565bb3da1fc88231e1212b3ae0718ee36a5
bhavarajus <s.bhavaraju@fintra.in>
2017-12-01 13:19:16 +0530

    moved back from 3000 to 4200 in protractor.conf.js and README.md

--- Fintra.Test/protractor.conf.js
+++ Fintra.Test/protractor.conf.js
@@ -1,5 +1,6 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
+
 const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
@@ -11,7 +12,7 @@ exports.config = {
         'browserName': 'chrome'
     },
     directConnect: true,
-    baseUrl: 'http://localhost:3000/',
+    baseUrl: 'http://localhost:4200/',
     framework: 'jasmine',
     jasmineNodeOpts: {
         showColors: true,
