454a2a97df387d5ecd6e5a93d7439e3d59e4e2f7
bhavarajus <s.bhavaraju@fintra.in>
2017-12-12 12:55:48 +0530

    rearranged folder structure.

--- Fintra.Test/src/app/app-routing.module.ts
+++ Fintra.Test/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from '../dashboard/dashboard.component';
-import { LoginComponent } from '../login/login.component';
-import { FilterCustomerComponent } from './customer/filterCustomer/filterCustomer.component';
-import { CustomerViewComponent } from './customer/customer-view/customer-view.component';
-import { TabCustomerComponent } from './customer/tabCustomer/tabCustomer.component';
-import { AccountComponent } from './customer/account/account.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { FilterCustomerComponent } from './Masters/customer/filterCustomer/filterCustomer.component';
+import { CustomerViewComponent } from './Masters/customer/customer-view/customer-view.component';
+import { TabCustomerComponent } from './Masters/customer/tabCustomer/tabCustomer.component';
+import { AccountComponent } from './Masters/customer/account/account.component';
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
--- Fintra.Test/src/app/app.module.ts
+++ Fintra.Test/src/app/app.module.ts
@@ -5,15 +5,15 @@ import { BsDatepickerModule } from 'ngx-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from '../login/login.component';
-import { DashboardComponent } from '../dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { FooterComponent } from '../common/footer/footer.component';
 import { HeaderComponent } from '../common/header/header.component';
-import { FilterCustomerComponent } from './customer/filterCustomer/filterCustomer.component';
-import { CustomerDataService } from './customer/customer-data.service';
-import { TabCustomerComponent } from './customer/tabCustomer/tabCustomer.component';
-import { CustomerViewComponent } from './customer/customer-view/customer-view.component';
-import { AccountComponent } from './customer/account/account.component';
+import { FilterCustomerComponent } from './Masters/customer/filterCustomer/filterCustomer.component';
+import { CustomerDataService } from './Masters/customer/customer-data.service';
+import { TabCustomerComponent } from './Masters/customer/tabCustomer/tabCustomer.component';
+import { CustomerViewComponent } from './Masters/customer/customer-view/customer-view.component';
+import { AccountComponent } from './Masters/customer/account/account.component';
 @NgModule({
   // tslint:disable-next-line:max-line-length
   declarations: [AppComponent, LoginComponent, HeaderComponent, FooterComponent, DashboardComponent, FilterCustomerComponent, CustomerViewComponent, TabCustomerComponent, AccountComponent],
