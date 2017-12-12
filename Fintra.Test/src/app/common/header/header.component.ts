a2d3486c5f35309bf753ccfc7f78b29d771436f4
bhavarajus <s.bhavaraju@fintra.in>
2017-12-12 16:04:33 +0530

    changed menu.

--- Fintra.Test/src/app/common/header/header.component.ts
+++ Fintra.Test/src/app/common/header/header.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+declare var $: any;
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   constructor(private router: Router) {}
     sideMenuOpened = true;
     openSideMenu = function() {
         this.sideMenuOpened = !this.sideMenuOpened;
     };
+    ngOnInit() {
+      $(document).ready(function() {
+        $('#menu').metisMenu();
+      });
+  }
 }
