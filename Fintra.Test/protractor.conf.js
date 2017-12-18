7682e2758526c948cab168a2aa65a5dcf49dbfa6
bhavarajus <s.bhavaraju@fintra.in>
2017-12-18 15:48:22 +0530

    added code for testing Masters.

--- Fintra.Test/protractor.conf.js
+++ Fintra.Test/protractor.conf.js
@@ -5,7 +5,7 @@ const { SpecReporter } = require('jasmine-spec-reporter');
 exports.config = {
     allScriptsTimeout: 11000,
     specs: [
-        './e2e/**/**/*.e2e-spec.ts'
+        './e2e/Masters/**/*.e2e-spec.ts'
     ],
     capabilities: {
         'browserName': 'chrome'
